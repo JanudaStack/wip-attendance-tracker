5682c79f59d3ea5bea446baaa4c49226c58593cf
JanudaStack <jaariyasinghe2003@gmail.com>
2026-01-05 23:05:10 +0530

    Implement security measures to disable right-click and keyboard shortcuts for inspecting elements

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// SECURITY SCRIPT (DISABLE RIGHT CLICK & INSPECT) ---
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+document.onkeydown = function (e) {
+  // F12
+  if (e.keyCode == 123) return false;
+
+  // Ctrl+Shift+I, Ctrl+Shift+J, Ctrl+Shift+C (Inspect Console)
+  if (
+    e.ctrlKey &&
+    e.shiftKey &&
+    (e.keyCode == 73 || e.keyCode == 74 || e.keyCode == 67)
+  )
+    return false;
+
+  // Ctrl+U (View Source)
+  if (e.ctrlKey && e.keyCode == 85) return false;
+};
+
 const defaultUsers = [
   "Chatura",
   "Henrietta",
